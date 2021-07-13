0c2348fa1d6eb42c02207589eb7ac6d23b1f3aea
OtavioJFOliveira <otaviojf_oliveira@outlook.com>
2021-07-12 23:29:38 -0300

    Install script

---
+++ install.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+echo "Installing Dependencies..."
+sudo apt net-tools
+sudo apt install portaudio19-dev python-all-dev
+sudo apt install python-pip
+sudo apt install gdebi
+
+echo "Installing Libraries"
+pip install gTTS
+pip install SpeechRecognition
+pip install playsound
+pip install pynput
+pip install PyAudio
+pip install unidecode
+pip install geopy
+
+
+echo "Installing Google Earth..."
+sudo gdebi google-earth-pro-stable_current_amd64.deb
+
+
+echo "Editing Drivers.Ini"
+
+sudo rm -f /opt/google/earth/pro/drivers.ini
+sudo mv drivers.ini /opt/google/earth/pro
+
+#python main.py
