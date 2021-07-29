4a15de58fab87b910dc0b6f66d96c9735781311f
OtavioJFOliveira <otaviojf_oliveira@outlook.com>
2021-07-29 20:50:44 -0300

    Audio settings added

--- install.sh
+++ install.sh
@@ -6,38 +6,47 @@ cat << "EOM"
 | | | | _  ___  _ | |_    | |  | |__    ___   | |  | |  ___   _ __ | |  __| |
 | | | || |/ __|| || __|   | |  | '_ \  / _ \  | |/\| | / _ \ | '__|| | / _` |
 \ \_/ /| |\__ \| || |_    | |  | | | ||  __/  \  /\  /| (_) || |   | || (_| |
- \___/ |_||___/|_| \__|   \_/  |_| |_| \___|   \/  \/  \___/ |_|   |_| \__,_|
+ \___/ |_||___/|_| \__|   |_|  |_| |_| \___|   \/  \/  \___/ |_|   |_| \__,_|
  
 https://github.com/LiquidGalaxyLAB/Visit-the-World
 -------------------------------------------------------------
+
+
 EOM
 
+#Sudo Access Start
+sudo -v
+
+echo ""
+
 # Parameters
-INSTALLATION_MODE="1"
+INSTALLATION_MODE="2"
 
 read -p "Installation Mode (1 == LattePanda) (2 == regular PC): " INSTALLATION_MODE
 
 
 if [ $INSTALLATION_MODE == "1" ]; then
-    echo ""
-    echo "YOU CHOSE OPTION $INSTALLATION_MODE, INSTALL ON LATTEPANDA BOARD"
-    echo ""
+    echo -e " \nYOU CHOSE OPTION $INSTALLATION_MODE, INSTALL ON LATTEPANDA BOARD \n"
     read -p "Press any key to continue or Ctrl+C to stop"
     echo "1" >ModeConfig.txt
 
 elif [ $INSTALLATION_MODE == "2" ]; then
-    echo ""
-    echo "YOU CHOSE OPTION $INSTALLATION_MODE, INSTALL ON A REGULAR PC"
-    echo ""
+    echo -e " \nYOU CHOSE OPTION $INSTALLATION_MODE, INSTALL ON A REGULAR PC \n"
     echo "2" >ModeConfig.txt
-    read -p "n Press any key to continue or Ctrl+C to stop"
+    read -p "Press any key to continue or Ctrl+C to stop"
 fi
 
-read -p "Parar teste" #apagar
 
-echo "Checking for system updates..."
+echo -e "\nAudio settings..."
+
+amixer -D pulse sset Master on   #Output Sound Turn on  
+amixer -D pulse sset Master 95%  #Output Sound level 
+amixer set Capture cap		 #Set Microphone capture On
+amixer set Capture 90%           #Microphone level 
+
+echo -e "\nChecking for system updates...\n"
 sudo apt update
-echo "Checking for system upgrades..."
+echo -e "\nChecking for system upgrades...\n"
 sudo apt upgrade -f
 
 echo "Installing Dependencies..."
@@ -58,7 +67,7 @@ pip install geopy
 pip install paramiko
 
 echo "Screen Dimming Setting"
-gsettings set org.gnome.desktop.session idle-delay 0
+gsettings set org.gnome.desktop.session idle-delay 0 #Set not to turn off screen 
 
 echo "Installing Google Earth..."
 sudo gdebi google-earth-pro-stable_current_amd64.deb
@@ -70,21 +79,23 @@ sudo cp ConfigFiles/drivers.ini /opt/google/earth/pro
 sudo rm -f google-earth-pro-stable_current_amd64.deb #Removing google-earth.deb
 
 
-chmod 755 GoogleEarthStart.sh
-chmod 755 i3config.sh
+sudo chmod 755 GoogleEarthStart.sh
+sudo chmod 755 i3config.sh
 
 # Only for installations on the LattePanda board
+#Enable the Serial communication 
 if [ $INSTALLATION_MODE == "1" ]; then
-    chmod o+rw /dev/ttyACM0 
+    sudo chmod o+rw /dev/ttyACM0 
 fi
 
 
-echo "Visit the World installation completed! :-)"
+echo -e "\nVisit the World installation completed! :-) \n"
 echo "Press ENTER key to reboot now"
 read
 google-earth-pro &
-sleep 10
+sleep 15
 
+killall googleearth-bin 
 reboot
 
 exit 0
