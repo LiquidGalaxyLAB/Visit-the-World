d9854cf1f56804b9361d407ab8aadeb5afa951af
OtavioJFOliveira <otaviojf_oliveira@outlook.com>
2021-07-29 20:52:40 -0300

    Audio settings added

--- GoogleEarthStart.sh
+++ GoogleEarthStart.sh
@@ -3,6 +3,13 @@
 sudo rm -f ~/.config/Google/GoogleEarthPro.conf
 sudo cp ~/Visit-the-World/ConfigFiles/GoogleEarthPro.conf ~/.config/Google
 
+amixer -D pulse sset Master on   #Output Sound Turn on  
+amixer -D pulse sset Master 95%  #Output Sound level 
+amixer set Capture cap		 #Set Microphone capture On
+amixer set Capture 90%           #Microphone level 
+
+killall googleearth-bin 
+
 google-earth-pro &
 
 python ~/Visit-the-World/main.py 
