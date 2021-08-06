8a859349ee8e77efcc25e0d9f05b2c68d6a23995
OtavioJFOliveira <otaviojf_oliveira@outlook.com>
2021-08-06 00:40:14 -0300

    Reload file to IP and Password to LG

---
+++ LG-reload.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+
+#rastart LGConfig File ####################################################
+
+f = open("ConfigFiles/LGConfig.txt", "w")
+f.write("a" + '\n')
+f.write("a")
+f.close()
+
+
