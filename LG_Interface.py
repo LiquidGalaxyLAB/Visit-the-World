de2e04f7d59a3797ecf486d20d05c655ac08380e
OtavioJFOliveira <otaviojf_oliveira@outlook.com>
2021-08-09 00:17:08 -0300

    Visual interface to LG Control

---
+++ LG_Interface.py
@@ -0,0 +1,71 @@
+#!/usr/bin/python
+
+import os
+from tkinter import *
+
+class Application:
+    def __init__(self, master=None):
+        self.fontePadrao = ("Arial", "10")
+        self.primeiroContainer = Frame(master)
+        self.primeiroContainer["pady"] = 20
+        self.primeiroContainer.pack()
+
+        self.segundoContainer = Frame(master)
+        self.segundoContainer["padx"] = 100
+        self.segundoContainer.pack()
+
+        self.terceiroContainer = Frame(master)
+        self.terceiroContainer["padx"] = 20
+        self.terceiroContainer.pack()
+
+        self.quartoContainer = Frame(master)
+        self.quartoContainer["pady"] = 30
+        self.quartoContainer.pack()
+
+        self.titulo = Label(self.primeiroContainer, text="LIQUID GALAXY CONNECTION")
+        self.titulo["font"] = ("Arial", "20", "bold")
+        self.titulo.pack()
+
+        self.nomeLabel = Label(self.segundoContainer,text="Master IP: ", font=self.fontePadrao)
+        self.nomeLabel.pack(side=LEFT)
+
+        self.nome = Entry(self.segundoContainer)
+        self.nome["width"] = 30
+        self.nome["font"] = self.fontePadrao
+        self.nome.pack(side=LEFT)
+
+        self.senhaLabel = Label(self.terceiroContainer, text="Master Password:", font=self.fontePadrao)
+        self.senhaLabel.pack(side=LEFT)
+
+        self.senha = Entry(self.terceiroContainer)
+        self.senha["width"] = 30
+        self.senha["font"] = self.fontePadrao
+        self.senha["show"] = "*"
+        self.senha.pack(side=LEFT)
+
+        self.autenticar = Button(self.quartoContainer)
+        self.autenticar["text"] = "Connect"
+        self.autenticar["font"] = ("Calibri", "8")
+        self.autenticar["width"] = 12
+        self.autenticar["command"] = self.verificaSenha
+        self.autenticar.pack()
+
+        self.mensagem = Label(self.quartoContainer, text="", font=self.fontePadrao)
+        self.mensagem.pack()
+
+    #Método verificar senha
+    def verificaSenha(self):
+        usuario = self.nome.get()
+        senha = self.senha.get()
+        if usuario == "Otavio" and senha == "admin":
+            self.mensagem["text"] = "Autenticado"
+            os.system("python ~/Visit-the-World/main.py ")
+        else:
+            self.mensagem["text"] = "Erro na autenticação"
+
+
+root = Tk()
+root.wm_title("Liquid Galaxy")
+root.geometry("800x500")
+Application(root)
+root.mainloop()
